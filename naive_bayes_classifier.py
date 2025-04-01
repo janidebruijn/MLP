01dfc967d3f61bef73e25aea274daf525fdf0df2
pc <t.y.thiescheffer@student.rug.nl>
2025-04-01 19:49:17 +0200

    added class weights

--- naive_bayes_classifier.py
+++ naive_bayes_classifier.py
@@ -5,6 +5,7 @@ import pandas as pd
 from tqdm import tqdm
 from sklearn import metrics
 from spacy.tokens import Doc
+from sklearn.utils import class_weight
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
 
@@ -33,11 +34,7 @@ def get_features(sentence_list):
     return df
 
 
-def mins(seconds):
-    return f'{seconds // 60:.0f} mins and {seconds % 60:.2f} secs'
-
-
-start = time.perf_counter()
+start = time.time()
 
 nlp_en = spacy.load(
     'en_core_web_sm', disable=['parser', 'attribute_ruler', 'lemmatizer'])
@@ -47,33 +44,48 @@ nlp_sp = spacy.load(
 train_sents = np.load('train_words.npy', allow_pickle=True)
 train_labels_sents = np.load('train_labels.npy', allow_pickle=True)
 
-amount = None
+n_sents = None
+batch_size = 10000
 model = MultinomialNB()
-train_feats = get_features(train_sents[:amount])
-train_labels = [label for sent in train_labels_sents[:amount]
+train_feats = get_features(train_sents[:n_sents])
+train_labels = [label for sent in train_labels_sents[:n_sents]
                 for label in sent]
 
 vectoriser = CountVectorizer()
-doc_term_matrix = vectoriser.fit_transform(train_feats['word'])
+word_vectors = vectoriser.fit_transform(train_feats['word'])
 train_feats = pd.concat([
     pd.DataFrame.sparse.from_spmatrix(
-        doc_term_matrix, columns=vectoriser.get_feature_names_out()),
-    train_feats.drop(columns=["word"])
+        word_vectors, columns=vectoriser.get_feature_names_out()),
+    train_feats.drop(columns=["word"]),
 ], axis=1)
+
+unique_classes = np.unique(train_labels)
+class_weights = class_weight.compute_class_weight(
+    'balanced', classes=unique_classes, y=train_labels)
+class_weights_dict = {
+    unique_classes[i]: class_weights[i]
+    for i in range(len(unique_classes))
+}
 
 indexes = [[index[0], index[-1] + 1] for index
            in np.array_split(range(train_feats.shape[0] + 1),
-                             len(train_feats) // 2500)]
+                             len(train_feats) // batch_size)]
 for start, end in tqdm(indexes, desc='Fitting'):
-    model.partial_fit(train_feats[start:end],
-                      train_labels[start:end],
-                      classes=np.unique(train_labels))
+    model.partial_fit(
+        train_feats[start:end],
+        train_labels[start:end],
+        classes=unique_classes,
+        sample_weight=[
+            class_weights_dict[label]
+            for label in train_labels[start:end]
+        ]
+    )
 
 dev_sents = np.load('dev_words.npy', allow_pickle=True)
 dev_labels_sents = np.load('dev_labels.npy', allow_pickle=True)
 
-dev_feats = get_features(dev_sents[:amount])
-dev_labels = [label for sent in dev_labels_sents[:amount] for label in sent]
+dev_feats = get_features(dev_sents[:n_sents])
+dev_labels = [label for sent in dev_labels_sents[:n_sents] for label in sent]
 
 doc_term_matrix = vectoriser.transform(dev_feats['word'])
 dev_feats = pd.concat([
@@ -84,7 +96,7 @@ dev_feats = pd.concat([
 
 indexes = [[index[0], index[-1] + 1] for index
            in np.array_split(range(dev_feats.shape[0] + 1),
-                             len(dev_feats) // 2500)]
+                             len(dev_feats) // batch_size)]
 
 dev_predictions = [
     prediction for start, end in tqdm(indexes, desc='Predicting')
@@ -94,5 +106,5 @@ dev_predictions = [
 print(metrics.classification_report(
     dev_labels, dev_predictions, digits=3, zero_division=0))
 
-time_taken = start - time.perf_counter()
-print(f'Took {mins(time_taken)}')
+time_taken = time.time() - start
+print(f'Took {time_taken // 60} minutes and {time_taken % 60} seconds')
